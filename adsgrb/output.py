996f160972de0bbe6c25f1f74fdb86270032af7c
Sam Young <youngsam@sas.upenn.edu>
2021-06-27 17:24:08 -0700

    Add keyword functionalty to lit search & fix various bugs

--- adsgrb/output.py
+++ adsgrb/output.py
@@ -1,10 +1,17 @@
 import os
 
 
-def savePDF(articlelist, output="./"):
+def savePDF(articlelist, output="./",debug=False):
     if isinstance(articlelist, str):
         return
     os.makedirs(os.path.dirname(output), exist_ok=True)
-    for article, title, year in articlelist:
-        with open(output + f"{year}_{title[0][:30].replace(' ', '_').replace('/','-')}.pdf", "wb") as f:
-            f.write(article)
+    for article in articlelist:
+        if len(article) == 4:
+            txt, title, year, url = article
+            with open(output + f"{year}_{title[0][:30].replace(' ','_').replace('/','-')}.pdf", "wb") as f:
+                f.write(txt)
+            if debug:
+                with open(output + f"{year}_{title[0][:30].replace(' ','_').replace('/','-')}.txt",'w') as dbg:
+                    dbg.write("URL: " + url)
+        else:
+            continue
