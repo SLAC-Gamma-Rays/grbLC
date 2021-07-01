54c0d2be8a5efdb88a1501cb14eae15a26816c78
Sam Young <youngsam@sas.upenn.edu>
2021-06-30 17:57:51 -0700

    Get rid of random misc files

--- adsgrb/__init__.py
+++ adsgrb/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Sam Young"""
 __email__ = "youngsam@sas.upenn.edu"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .search import gcnSearch, litSearch, getArticles
 from .output import savePDF
