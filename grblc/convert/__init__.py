7855c028d1fae4cb05eb1d4b287580f64008acd1
Sam Young <youngsam@sas.upenn.edu>
2021-07-17 23:46:04 -0700

    Heavy refactor conversion module; same usage tho

--- grblc/convert/__init__.py
+++ grblc/convert/__init__.py
@@ -1,1 +1,9 @@
-from .convert import *
+""" TO USE:
+Import this file into a directory to be used in your command line interfance (e.g., terminal on mac, cmd.exe on windows)
+Make sure you are in the directory, and then start Jupyter notebook or Jupyter lab / any other Python instance from there
+and import as any other module.
+"""
+
+
+from .convert import convertGRB, save_convert_params, setdir, getdir, directory
+from .time import dec_to_UT, UT_to_dec, grb_to_date
