829882071af7b04b15aa28c3db7593effa062608
Sam Young <youngsam@sas.upenn.edu>
2022-01-06 21:57:28 -0800

    add __init__.py

---
+++ grblc/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for grbLC submodule."""
+
+__author__ = """Sam Young"""
+__email__ = "youngsam@sas.upenn.edu"
+__version__ = "0.0.0"
+
+from . import convert, fitting, search  # noqa F401
