713fd869e16c3d983930a08fd1690d926bde8a30
Sam Young <youngsam@sas.upenn.edu>
2022-01-06 21:30:15 -0800

    changes to travis config, ,

--- setup.py
+++ setup.py
@@ -1,53 +1,45 @@
 #!/usr/bin/env python
-
 """The setup script."""
+import os
+import re
+import sys
 
-from setuptools import setup, find_packages
-import re, os, sys
+from setuptools import find_packages
+from setuptools import setup
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ads", "requests"]
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read().splitlines()
 
 test_requirements = []
 
 # Thank you Andy Casey for this nice versioning method
 major, minor1, minor2, release, serial = sys.version_info
-
-
 readfile_kwargs = {"encoding": "utf-8"} if major >= 3 else {}
 
 
-def readfile(filename):
-    with open(filename, **readfile_kwargs) as fp:
-        contents = fp.read()
-    return contents
-
-
 version_regex = re.compile('__version__ = "(.*?)"')
-contents = readfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), "adsgrb", "__init__.py"))
-
-version = version_regex.findall(contents)[0]
-
+with open(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "__init__.py"),
+    **readfile_kwargs
+) as fp:
+    contents = fp.read()
+    version = version_regex.findall(contents)[0]
 
 setup(
     author="Sam Young",
     author_email="youngsam@sas.upenn.edu",
     python_requires=">=3.6",
-    description="A simple way to scour the ADS for GRB data.",
-    entry_points={
-        "console_scripts": [
-            "adsgrb=adsgrb.cli:main",
-        ],
-    },
+    description="A Python tool for searching GRB light-curve studies",
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
     include_package_data=True,
-    keywords="adsgrb",
-    name="adsgrb",
-    packages=find_packages(include=["adsgrb", "adsgrb.*"]),
+    keywords="grblc",
+    name="grblc",
+    packages=find_packages(include=["grblc", "grblc.*"]),
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/youngsm/adsgrb",
