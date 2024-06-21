332cb90ab840970053c0c3fa119dc2f98ac0fd50
Ridha <ridhafathima273@gmail.com>
2024-06-21 13:50:55 +0200

    updated requirements.txt

--- setup.py
+++ setup.py
@@ -4,32 +4,27 @@ import os
 import re
 import sys
 from functools import reduce
+from setuptools import find_packages, setup
 
-from setuptools import find_packages
-from setuptools import setup
-
-with open("README.rst") as readme_file:
+# Read the README file
+with open("README.rst", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open("requirements.txt") as requirements_file:
-    requirements = requirements_file.readlines()
+# Read the requirements file
+with open("requirements.txt", "r", encoding="utf-8") as requirements_file:
+    requirements = [line.strip() for line in requirements_file.readlines()]
 
-# with open("requirements_dev.txt") as requirements_test_file:
-#     test_requirements = requirements_test_file.readlines()
+with open("requirements_dev.txt", "r", encoding="utf-8") as requirements_test_file:
+    test_requirements = [line.strip() for line in requirements_test_file.readlines()]
 
-# Thank you Andy Casey for this nice versioning method
-major, minor1, minor2, release, serial = sys.version_info
-readfile_kwargs = {"encoding": "utf-8"} if major >= 3 else {}
-
-
+# Version extraction method
 version_regex = re.compile('__version__ = "(.*?)"')
 with open(
     reduce(os.path.join, [os.path.dirname(os.path.abspath(__file__)), "grblc", "__init__.py"]),
-    **readfile_kwargs
+    encoding="utf-8"
 ) as fp:
     contents = fp.read()
     version = version_regex.findall(contents)[0]
-
 
 setup(
     author="Ridha Fathima Mohideen Malik",
@@ -41,22 +36,21 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="A Python package for GRB optical light curve studies.",
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type='text/x-rst',
     keywords="grblc",
     name="grblc",
     packages=find_packages(include=["grblc", "grblc.*"]),
-    package_dir={"": "grbLC"},
     include_package_data=True,
     package_data={
-        "": ["*.txt"],
-        "": ["*.csv"]
+        "": ["*.txt", "*.csv"],
     },
     test_suite="tests",
     # tests_require=test_requirements,
